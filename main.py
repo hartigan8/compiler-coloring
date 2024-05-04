8683a450ea4b7c54f0f026d2ac52d81558ccf9bb
ekinbayri <ekinbayri@gmail.com>
2024-05-04 15:32:47 +0300

    Return statements

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import sys
 import clang.cindex
 
 class Line:
-    def __init__(self, decl, number):
+    def __init__(self, decl, number,return_stmt):
         self.decl = decl
         self.refs = []
         self.number = number
+        self.return_stmt = return_stmt
 
 def find_line(lines, number):
     for line in lines:
@@ -15,10 +16,11 @@ def find_line(lines, number):
 
 def traverse(node, lines):
     if(node.kind == clang.cindex.CursorKind.RETURN_STMT):
+        lines.append(Line(node.displayname, node.location.line,True))
         print("Return statement found at line: %d" % node.location.line)
         
     if  node.kind == clang.cindex.CursorKind.VAR_DECL:
-        lines.append(Line(node.displayname, node.location.line))
+        lines.append(Line(node.displayname, node.location.line,False))
 
     if node.kind == clang.cindex.CursorKind.DECL_REF_EXPR:
         line = find_line(lines, node.location.line)
@@ -35,7 +37,7 @@ def main():
     tu = index.parse("test.c", args=['-x', 'c'])
     traverse(tu.cursor, lines)
     for line in lines:
-        print("line: %d, decl: %s, refs: %s" % (line.number, line.decl, line.refs))
+        print("line: %d, decl: %s, refs: %s , return: %s" % (line.number, line.decl, line.refs, line.return_stmt))
 
 if __name__ == '__main__':
     main()
