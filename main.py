b20fc5495f1d43e10af72b4040b62b966cfeeca5
hartiganm8 <m.can-yilmaz@outlook.com>
2024-05-04 15:57:53 +0300

    function based

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 import sys
 import clang.cindex
 
+
+
+
+
+
+class Function:
+    def __init__(self, name):
+        self.name = name
+        self.lines = []
+    
+    def print(self):
+        for line in self.lines:
+            print("line: %d, decl: %s, refs: %s , return: %s" % (line.number, line.decl, line.refs, line.return_stmt))
+
+    def find_line(self, lines, number):
+        for line in lines:
+            if line.number == number:
+                return line
+        return None
+    
+
 class Line:
     def __init__(self, decl, number,return_stmt):
         self.decl = decl
@@ -8,36 +29,40 @@ class Line:
         self.number = number
         self.return_stmt = return_stmt
 
-def find_line(lines, number):
-    for line in lines:
-        if line.number == number:
-            return line
-    return None
+    
 
-def traverse(node, lines):
+
+
+def traverse(node, functions, current_func = ""):
+    if(node.kind == clang.cindex.CursorKind.FUNCTION_DECL):
+        functions[node.spelling] = Function(node.displayname)
+        current_func = node.spelling
+        
     if(node.kind == clang.cindex.CursorKind.RETURN_STMT):
-        lines.append(Line(node.displayname, node.location.line,True))
-        print("Return statement found at line: %d" % node.location.line)
+        functions[current_func].lines.append(Line(node.displayname, node.location.line,True))
         
     if  node.kind == clang.cindex.CursorKind.VAR_DECL:
-        lines.append(Line(node.displayname, node.location.line,False))
+        functions[current_func].lines.append(Line(node.displayname, node.location.line,False))
 
     if node.kind == clang.cindex.CursorKind.DECL_REF_EXPR:
-        line = find_line(lines, node.location.line)
+        fun = functions[current_func]
+        line = fun.find_line(functions[current_func].lines, node.location.line)
         if line:
             line.refs.append(node.displayname)
     for child in node.get_children():
-        traverse(child, lines)
+        traverse(child, functions, current_func)
 
 
 
 def main():
-    lines = []
+    functions = {}
     index = clang.cindex.Index.create()
     tu = index.parse("test.c", args=['-x', 'c'])
-    traverse(tu.cursor, lines)
-    for line in lines:
-        print("line: %d, decl: %s, refs: %s , return: %s" % (line.number, line.decl, line.refs, line.return_stmt))
+    traverse(tu.cursor, functions)
+    for func in functions.values():
+        func.print()
+        print("\n")
+
 
 if __name__ == '__main__':
     main()
--- test.c
+++ test.c
@@ -1,7 +1,14 @@
+int test(){
+    int b = 1;
+    int a = 2;
+    int c = a + b;
+    return c;
+}
+
 int main(){
     int a = 1;
-    int b = 2 + a;
-    int c = 3;
+    int b = 2 + a + test();
+    int c = 3 + a + b;
     if(a == 1){
         if(b == 2){
             if(c == 3){
@@ -11,9 +18,4 @@ int main(){
     }
     return 1;
 }
-int test(){
-    int b = 1;
-    int a = 2;
-    int c = a + b;
-    return c;
-}
+
