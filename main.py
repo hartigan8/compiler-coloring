beb5dad0c86fff2cc01e792d271471dd581a79d2
hartiganm8 <m.can-yilmaz@outlook.com>
2024-05-04 19:49:39 +0300

    turning to graph

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import sys
 import clang.cindex
 import networkx as nx
-
+import matplotlib.pyplot as plt
 
 
 class Function:
     def __init__(self, name):
         self.name = name
         self.lines = []
+        self.alive_values = []
+        self.decl_list = []
     
     def print(self):
         for line in self.lines:
@@ -18,10 +20,12 @@ class Function:
             if line.number == number:
                 return line
         return None
-
+    def reverse(self):
+        self.lines.reverse()
+    
 
 class Line:
-    def __init__(self, decl, number,return_stmt):
+    def __init__(self, decl, number, return_stmt):
         self.decl = decl
         self.refs = []
         self.number = number
@@ -31,24 +35,33 @@ class Line:
 
 
 
-def traverse(node, functions, current_func = ""):
-    
-    
+def traverse(node, functions, parent=None, depth = 0, current_func = ""):
+    '''
     if(node.kind == clang.cindex.CursorKind.IF_STMT):
         children = list(node.get_children())
         print(node.location.line)
+        depth = depth + 1
         for child in children:
-            print(f"{child.kind} {child.displayname} {child.location.line} {child.location.offset}")
+            print(f"{depth} {child.kind} {child.displayname} {child.location.line} {child.location.offset}")
+    '''
+    
+        
 
     if(node.kind == clang.cindex.CursorKind.FUNCTION_DECL):
         functions[node.spelling] = Function(node.displayname)
         current_func = node.spelling
 
     if(node.kind == clang.cindex.CursorKind.RETURN_STMT):
-        functions[current_func].lines.append(Line(node.displayname, node.location.line,True))
-        
+        functions[current_func].lines.append(Line(node.displayname, node.location.line, True))
+    
+    if(node.location.line == 5 ):
+        print(parent.kind)
+        print(f"{node.kind} {node.spelling}")
+        print()
+    
     if  node.kind == clang.cindex.CursorKind.VAR_DECL:
-        functions[current_func].lines.append(Line(node.displayname, node.location.line,False))
+        functions[current_func].lines.append(Line(node.displayname, node.location.line, False))
+        functions[current_func].decl_list.append(node.displayname)
 
     if node.kind == clang.cindex.CursorKind.DECL_REF_EXPR:
         fun = functions[current_func]
@@ -56,8 +69,53 @@ def traverse(node, functions, current_func = ""):
         if line:
             line.refs.append(node.displayname)
     for child in node.get_children():
-        traverse(child, functions, current_func)
+        traverse(child, functions, node, depth, current_func)
 
+
+def liveliness_analysis(functions):
+    vals = list(functions.values())
+
+    for func in vals:
+        alive_values = []
+        func.reverse()
+        func.print()
+        i = 0
+        for line in func.lines:
+            if len(alive_values) == 0:
+                alive_values.append(line.refs)
+            else:
+                if line.decl in alive_values[i - 1]:
+                    prev = alive_values[i - 1].copy()
+                    prev.remove(line.decl)
+                    for ref in line.refs:
+                        if ref not in prev:
+                            prev.append(ref)
+                    alive_values.append(prev)
+                else:
+                    prev = alive_values[i - 1].copy()
+                    for ref in line.refs:
+                        if ref not in prev:
+                            prev.append(ref)
+                    alive_values.append(prev)
+            i = i + 1
+        func.alive_values = alive_values
+        print(alive_values)
+        print("\n")
+        
+
+def to_graph(functions):
+    G = nx.Graph()
+    for func in functions.values():
+        decl_list = func.decl_list
+        for decl in decl_list:
+            G.add_node(decl)
+            for alive in func.alive_values:
+                if decl in alive:
+                    for decl2 in alive:
+                        if decl2 != decl:
+                            G.add_edge(decl, decl2)
+    nx.draw(G, with_labels=True)
+    plt.show()
 
 
 def main():
@@ -73,9 +131,9 @@ def main():
 
 
     traverse(tu.cursor, functions)
-    for func in functions.values():
-        func.print()
-        print("\n")
+    
+    liveliness_analysis(functions)
+    to_graph(functions)
 
 
 if __name__ == '__main__':
--- test.c
+++ test.c
@@ -2,43 +2,14 @@ int test(){
     int b = 1;
     int a = 2;
     int c = a + b;
+    b = c + a;
     return c;
 }
 
 int main(){
     int a = 1;
-    int b = 2 + a + test();
+    int b = 2 + a;
     int c = 3 + a + b;
-    if(1== 1){
-        if(a == 1){
-            if(b == 2){
-                if(c == 3){
-                    return 0;
-                }
-                else if(c == 4){
-                    return 1;
-                }
-                else{
-                    return 2;
-                }
-            }
-            else if(b == 4){
-                return 1;
-            }
-            else if(b == 3){
-                return 1;
-            }
-            else{
-                return b;
-            }
-        }
-        else if(a == 2){
-            return a;
-        }
-        else{
-            return 1;
-        }
-    }
-    return 1;
+    return c;
 }
 
